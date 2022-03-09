6387c2a3cb212bb08c4b671d7686b1bcf3965d97
Ross Weijer <ross@datascopeanalytics.com>
2022-03-09 17:36:57 -0600

    Rectangle guessing game has janky GUI output using turtle module

--- geometry-game/game.py
+++ geometry-game/game.py
@@ -1,24 +1,33 @@
-from geometries import Point, Rectangle
+import turtle
+
+from geometries import Point, Rectangle, VisualPoint, VisualRectangle
 from random import randint
+
+RANDINT_RANGE = 150
+POINT_SIZE = 10
+POINT_COLOR = "#E921CD"
 
 
 if __name__ == "__main__":
 
-    ll_x = randint(0, 15)
-    ll_y = randint(0, 15)
+    ll_x = randint(0, RANDINT_RANGE)
+    ur_x = ll_x + randint(0, RANDINT_RANGE)
+    ll_y = randint(0, RANDINT_RANGE)
+    ur_y = ll_y + randint(0, RANDINT_RANGE)
     lower_left = Point(x = ll_x, y = ll_y)
-    upper_right = Point(x = ll_x + randint(0, 15), y = ll_y + randint(0,15))
-    rectangle = Rectangle(lower_left = lower_left, upper_right = upper_right)
+    upper_right = Point(x = ur_x, y = ur_y)
+    rectangle = VisualRectangle(lower_left = lower_left, 
+        upper_right = upper_right)
 
     print(f"For this game, imagine a {rectangle}")
     print("Your mission is to guess a point inside this rectangle!")
 
-    new_x = float(input("Please choose an x coordinate for your point: "))
-    new_y = float(input("Now please choose a y coordinate for your point: "))
+    user_x = float(input("Please choose an x coordinate for your point: "))
+    user_y = float(input("Now please choose a y coordinate for your point: "))
+    user_point = VisualPoint(x = user_x, y = user_y, 
+        size = POINT_SIZE, hex_code = POINT_COLOR)
 
-    new_point = Point(x = new_x, y = new_y)
-
-    if rectangle.contains(new_point):
+    if rectangle.contains(user_point):
         print("Congratulations! You Win!")
         print("\nNow, try guessing the area of the rectangle")
         guess = float(input("I think the area is... "))
@@ -29,4 +38,11 @@ if __name__ == "__main__":
 
     else:
         print("Woof...Please Try Again...")
+
+
+    game_canvas = turtle.Turtle()
+    rectangle.draw(canvas = game_canvas)
+    user_point.draw(canvas = game_canvas)
+    turtle.done()
+
 
