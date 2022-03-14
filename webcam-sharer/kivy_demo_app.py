80ea92e977aff1ef671fb673568f30221b9e63ee
Ross Weijer <ross@datascopeanalytics.com>
2022-03-14 11:25:26 -0500

    Kivy demo only downloads pngs and jpgs

--- webcam-sharer/kivy_demo_app.py
+++ webcam-sharer/kivy_demo_app.py
@@ -7,6 +7,7 @@ from kivy.app import App
 from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
 from urllib.parse import quote
+from typing import List
 
 IMAGES_DIR = "images"
 
@@ -15,48 +16,60 @@ class FirstScreen(Screen):
 
 
     def get_wikipedia_image_url(self, image_query:str, 
-        take_first:bool = False) -> str:
-        '''By default we pull the first image found on the wikipedia page. Set 
-        take_first to False to choose randomly from all images.
+        file_types:List[str]="jpg, png") -> str:
+        '''Chooses a random image from a wikiped
         '''
         wiki_page = wikipedia.page(image_query)
         wiki_images = wiki_page.images
-        if take_first:
-            print(wiki_images[0])
-            return wiki_images[0]
-        return random.choice(wiki_images)
+        chosen_url = None
+        while chosen_url is None:
+            url_guess = random.choice(wiki_images)
+            url_file_type = url_guess.split(".")[-1]
+            if url_file_type in file_types:
+                chosen_url = url_guess
+        return chosen_url
+
+
+    def get_download_path(self, image_url:str, image_query: str, 
+        download_dir: str = "images") -> pathlib.Path:
+        '''Returns the local path where we will download a wikipedia image
+        '''
+        base_dir = pathlib.Path(download_dir)
+        image_fname = quote(image_query)
+        image_ext = image_url.split(".")[-1]
+        download_name = f"{image_fname}.{image_ext}"
+        return base_dir / download_name
 
 
     def download_image(self, image_url:str, image_query: str, 
-        download_dir: str = "images"):
+        download_path: pathlib.Path) -> bool:
         '''Once we have an image url, we can download it and save it to show 
-        the result in our application. Returns the path where the image was
-        downloaded 
+        the result in our application. Returns a boolean indicating whether
+        or not the download was successful 
+
+        Kind of hate the fact that this returns a boolean
         '''
-        image_ext = image_url.split(".")[-1]
-        image_fname = quote(image_query)
-        base_dir = pathlib.Path(download_dir)
         print(f"downloading {image_url}")
         headers = {"User-agent": "Chrome/99.0.4844.51"}
         image_data = requests.get(image_url, headers = headers)
         http_status = image_data.status_code
         if http_status == 200:
-            download_name = f"{image_fname}.{image_ext}"
-            download_path = base_dir / download_name
             with open(download_path, "wb") as f:
                 f.write(image_data.content)
-                print(f"Image downloaded to {download_path}")
-                return str(download_path)
+                return True
         print(f"Error downloading image (code {http_status}) ... Sad Shrek")
-        return str(base_dir / "sad-shrek.jpg")
+        return False
 
 
-    def search_image(self):
+    def search_image(self, fallback_path:str = "images/sad-shrek.jpg"):
         img_query = self.manager.current_screen.ids.txt.text
         img_url = self.get_wikipedia_image_url(image_query = img_query)
-        result_path = self.download_image(image_url = img_url, 
+        dl_path = self.get_download_path(image_url = img_url, 
             image_query = img_query)
-        self.manager.current_screen.ids.img.source = result_path
+        did_download = self.download_image(image_url = img_url, 
+            image_query = img_query, download_path = dl_path)
+        result = str(dl_path) if did_download else fallback_path
+        self.manager.current_screen.ids.img.source = result
 
 
 class RootWidget(ScreenManager):
